a77f35682d9e13a91c8332c9f347e6c476a09ee0
Fabian Wetzel <fabsenet@googlemail.com>
2015-09-09 20:44:32 +0200

    replaced listbox with listview; made sample running endlessly again

--- IotActor/MainPage.xaml.cs
+++ IotActor/MainPage.xaml.cs
@@ -12,29 +12,35 @@ namespace IotActor
 
         private readonly ObservableCollection<LedOnOffSwitch> _ledOnOffSwitches = new ObservableCollection<LedOnOffSwitch>();
         private Task _ledSwitchingTask;
+        private CancellationTokenSource _token;
 
         public MainPage()
         {
             this.InitializeComponent();
 
             //_controller = new AmqpEndpointController();
+
+
             DataContext = _ledOnOffSwitches;
-            var token = new CancellationTokenSource();
-            var led = new LedOnOffSwitch(18, token.Token, Dispatcher);
+            _token = new CancellationTokenSource();
+            var led = new LedOnOffSwitch(18, _token.Token, Dispatcher);
             _ledOnOffSwitches.Add(led);
             _ledSwitchingTask = Task.Factory.StartNew(() =>
             {
-                for (int i = 0; i < 20; i++)
+                while (true)
                 {
-                    led.SetState(i%2 == 0);
-                    Task.Delay(TimeSpan.FromMilliseconds(500)).Wait();
+                    for (int i = 0; i < 20; i++)
+                    {
+                        led.SetState(i%2 == 0);
+                        Task.Delay(TimeSpan.FromMilliseconds(300)).Wait();
+                    }
+                    for (int i = 0; i < 4; i++)
+                    {
+                        led.SetState(i%2 == 0);
+                        Task.Delay(TimeSpan.FromMilliseconds(3000)).Wait();
+                    }
                 }
-                for (int i = 0; i < 4; i++)
-                {
-                    led.SetState(i%2 == 0);
-                    Task.Delay(TimeSpan.FromMilliseconds(5000)).Wait();
-                }
-            });
+            }, _token.Token);
         }
 
     }
